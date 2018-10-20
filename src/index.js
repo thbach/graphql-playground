ec50bc4aca191cc55630066db293d52b60809e5d
tbach <thbach@gmail.com>
2018-10-20 00:49:39 -0700

    added delete and create mutations

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import {GraphQLServer} from 'graphql-yoga'
+import uuidv4 from 'uuid/v4'
 
 //demo data
-const users = [
+let users = [
   {
     id: '1',
     name: 'thai',
@@ -21,7 +22,7 @@ const users = [
   }
 ]
 
-const posts = [
+let posts = [
   {
     id: '1',
     title: 'hellow world',
@@ -45,7 +46,7 @@ const posts = [
   }
 ]
 
-const comments = [
+let comments = [
   {id: 'rf234', text: 'this is a stupid comment', post: '1', author: '2'},
   {id: 'r423f', text: 'your the best in the world', post: '2', author: '2'},
   {id: 'f34', text: 'you know it', post: '3', author: '1'},
@@ -59,6 +60,34 @@ const typeDefs = `
     comments(query: String): [Comment!]!
     me: User!
     post: Post!
+  }
+
+  type Mutation {
+    createUser(data: CreateUserInput!): User!
+    createPost(data: CreatePostInput!): Post!
+    createComment(data: CreateCommentInput!): Comment!
+    deleteUser(id: String!): User!
+    deletePost(id: String!): Post!
+    deleteComment(id: String!): Comment!
+  }
+
+  input CreateUserInput {
+    name: String!,
+    email: String!,
+    age: Int
+  }
+
+  input CreatePostInput {
+    title: String!,
+    body: String!,
+    published: Boolean!,
+    author: String!,    
+  }
+
+  input CreateCommentInput {
+    text: String!,
+    post: String!,
+    author: String!
   }
 
   type Post {
@@ -131,6 +160,80 @@ const resolvers = {
         body: 'whats up doc',
         published: false
       }
+    }
+  },
+  Mutation: {
+    createUser(parent, args, ctx, info) {
+      const emailTaken = users.some(user => user.email === args.data.email)
+      if (emailTaken) {
+        throw new Error('email taken')
+      }
+      const user = {
+        id: uuidv4(),
+        ...args.data
+      }
+      users.push(user)
+      return user
+    },
+    createPost(parent, args, ctx, info) {
+      const userExist = users.some(user => user.id === args.data.author)
+      if (!userExist) {
+        throw new Error('User not found')
+      }
+      const post = {
+        id: uuidv4(),
+        ...args.data
+      }
+      posts.push(post)
+      return post
+    },
+    createComment(parent, args, ctx, info) {
+      const userExist = users.some(user => user.id === args.data.author)
+      const postExist = posts.some(post => post.id === args.data.post && post.published)
+      if (!userExist) {
+        throw new Error('user not found')
+      }
+      if (!postExist) {
+        throw new Error('post not found')
+      }
+      const comment = {
+        id: uuidv4(),
+        ...args.data
+      }
+      comments.push(comment)
+      return comment
+    },
+    deleteUser(parent, args, ctx, info) {
+      const userIndex = users.findIndex(user => user.id === args.id)
+      if (userIndex === -1) {
+        throw new Error('could not find user')
+      }
+      const deletedUser = users.splice(userIndex, 1)
+      posts = posts.filter(post => {
+        const match = post.author === args.id
+        if (match) {
+          comments = comments.filter(comment => comment.post !== post.id)
+        }
+        return !match
+      })
+      comments = comments.filter(comment => comment.author !== args.id)
+      return deletedUser[0]
+    },
+    deletePost(parent, args, ctx, info) {
+      const postIndex = posts.findIndex(post => post.id === args.id)
+      if (postIndex === -1) {
+        throw new Error('could not find post')
+      }
+      const deletedPost = posts.splice(postIndex, 1)
+      comments = comments.filter(comment => comment.post !== args.id)
+      return deletedPost[0]
+    },
+    deleteComment(parent, args, ctx, info) {
+      const commentIndex = comments.findIndex(comment => comment.id === args.id)
+      if (commentIndex === -1) {
+        throw new Error('could not find comment')
+      }
+      return comments.splice(commentIndex, 1)[0]
     }
   },
   Post: {
